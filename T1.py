bed7b6e14ac8500698acef103880cafd0296a7da
Wieland Vandebotermet <71000992+WielandVandebotermet@users.noreply.github.com>
2023-10-04 15:19:41 +0200

    Add files via upload

---
+++ T1.py
@@ -0,0 +1,103 @@
+
+#Importeert de benodigheden
+from constraint import *
+from simpleai.search import CspProblem, backtrack
+import streamlit as st
+
+st.title('Cryptarithmetic Task')
+
+#De Benodigd heden om te weten wat bereken
+Word1 = "AB"
+Word2 = "C"
+Result = "AAA"
+#om te weten hoe we deze bereken
+Type = "1"
+
+
+variables = ()
+#voegt alle unieke letter toe in variables(tuples)
+for letter in range(len(Word1)):
+    if Word1[letter] not in variables:
+        variables += (Word1[letter],)
+
+for letter in range(len(Word2)):
+    if Word2[letter] not in variables:
+        variables += (Word2[letter],)
+
+for letter in range(len(Result)):
+    if Result[letter] not in variables:
+        variables += (Result[letter],)
+
+domains = {}
+#voegt alle variabele, toe in domains(dict) en geeft ze een list mee van 1 tot 9 of 0 tot 9. Afhankelijk of het de eerste letter is of niet
+if(len(variables) > 2):
+    for letter in range(len(variables)):
+        if((variables[letter] == Word1[0]) or (variables[letter] == Word2[0]) or (variables[letter] == Result[0])):
+            domains[variables[letter]] = list(range(1, 10))
+        else:
+            domains[variables[letter]] = list(range(0, 10))
+
+
+#De functie zorgt ervoor dat ze uniek zijn
+def constraint_unique(variables, values):
+    return len(values) == len(set(values))
+
+#De functie koppelt waarden aan letters en check of deze opgeteld kunnen worden 
+def constraint_add(variables, values):
+    factor1 = ""
+    factor2 = ""
+    result= ""  
+    for letter in Word1:
+        factor1 += str(values[variables.index(letter)])
+    for letter in Word2:
+        factor2 += str(values[variables.index(letter)])
+    for letter in Result:
+        result += str(values[variables.index(letter)])
+    return (int(factor1) + int(factor2)) == int(result)
+
+#De functie koppelt waarden aan letters en check of deze afgetrokken kunnen worden 
+def constraint_det(variables, values):
+    factor1 = ""
+    factor2 = ""
+    result= ""  
+    for letter in Word1:
+        factor1 += str(values[variables.index(letter)])
+    for letter in Word2:
+        factor2 += str(values[variables.index(letter)])
+    for letter in Result:
+        result += str(values[variables.index(letter)])
+    return (int(factor1) - int(factor2)) == int(result)
+
+#De functie koppelt waarden aan letters en check of deze vermenigdvuldig kunnen worden 
+def constraint_mulp(variables, values):
+    factor1 = ""
+    factor2 = ""
+    result= ""  
+    for letter in Word1:
+        factor1 += str(values[variables.index(letter)])
+    for letter in Word2:
+        factor2 += str(values[variables.index(letter)])
+    for letter in Result:
+        result += str(values[variables.index(letter)])
+    return (int(factor1) * int(factor2)) == int(result)
+
+#Dit zorgt ervoor dat de juiste berekening wordt berekend
+if(Type == "1"):
+    constraints = [
+        (variables, constraint_unique),
+        (variables, constraint_mulp),
+    ]
+elif(Type == "2"):
+    constraints = [
+        (variables, constraint_unique),
+        (variables, constraint_det),
+    ]
+else:
+    constraints = [
+        (variables, constraint_unique),
+        (variables, constraint_add),
+    ]
+
+#dit berekend dan de oplossing
+output = backtrack(CspProblem(variables, domains, constraints))
+print('\nSolutions:', output)
